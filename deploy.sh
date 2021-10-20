8fc60945f83580862ede7c3df3c7b38169315e5b
jemin <jemin03120111@gmail.com>
2021-10-20 13:44:24 +0900

    .

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd /home/ubuntu/deploy/se_project/
+cd /home/ubuntu/deploy/se_project
 npm start 
---
+++ execute-deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /home/ubuntu/se_project
+./deploy.sh > /dev/null 2> /dev/null < /dev/null &
