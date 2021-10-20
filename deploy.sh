37ff5eb7f976691b87e644674e869eb87b91d7ef
jemin <jemin03120111@gmail.com>
2021-10-20 14:47:03 +0900

    test...

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /home/ubuntu/deploy/se_project
-npm install 
-npm start 
--- execute-deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /home/ubuntu/se_project
-sh ./deploy.sh > /dev/null 2> /dev/null < /dev/null &
